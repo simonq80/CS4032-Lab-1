ee18a266c819056d9068ef937b77189b0938220a
simonq80 <simonq80@gmail.com>
2016-11-02 19:51:00 +0000

    built with stack, some minor fixes

---
+++ Lab1Stack/src/Main.hs
@@ -0,0 +1,28 @@
+module Main where
+import System.IO
+import qualified Data.ByteString.Char8 as B8
+import Network.HTTP.Base (urlEncode)
+import Network.Socket hiding (send, recv)
+import Network.Socket.ByteString
+import Options.Applicative
+
+main = do
+	s <- createConnection "localhost" 8000
+	m <- sendMess s "testtest"
+	B8.putStrLn m
+
+createConnection:: String -> Int -> IO Socket
+createConnection h p = do 
+	addrInfo : _ <- getAddrInfo Nothing (Just h) (Just (show p))
+	s <- socket (addrFamily addrInfo) Stream defaultProtocol
+	connect s (addrAddress addrInfo)
+	return s
+
+
+sendMess :: Socket -> String -> IO B8.ByteString
+sendMess s m = do
+  send s (B8.pack $ getRequest m)
+  recv s 4096
+
+getRequest :: String -> String
+getRequest s = "GET /echo.php?message=" ++ s ++ " HTTP/1.1"
